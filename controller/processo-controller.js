fc28c89e20fdee559af180362cade6e3f3880f2c
GustavoShoji <shoji132000@outlook.com>
2023-06-05 11:44:59 -0300

    1.13

--- controller/processo-controller.js
+++ controller/processo-controller.js
@@ -18,7 +18,7 @@ exports.getProcessos = async (req, res, next) => {
 
 //Seleciona tudo
 exports.getProcessoTudo = async (req, res, next) => {
-    const query = ` SELECT p.numero, YEAR(p.Data_de_cadastro), Month(p.Data_de_cadastro), p.Objeto, p.Justificativa, p.Valor_Solicitado, p.MA, p.Etapa_Atual, 
+    const query = ` SELECT p.numero, YEAR(p.Data_de_cadastro) as Ano, Month(p.Data_de_cadastro) as Mês, p.Objeto, p.Justificativa, p.Valor_Solicitado, p.MA, p.Etapa_Atual, 
                     b.Cnpj_beneficiario, b.Nome_beneficiario, b.Uf_beneficiario,
                     a.Tipo_autor, a.Autor,
                     o.Nome_orgao, o.Cod_orgao, o.Nome_uo, o.Cod_uo
@@ -160,11 +160,15 @@ exports.getProcessoPorAutorAno = async (req, res, next) => {
                   SUM(p.Valor_Solicitado) AS valor_total_processos, 
                   COUNT(DISTINCT b.Nome_beneficiario) AS quantidade_beneficiarios,
                   COUNT(DISTINCT b.Uf_beneficiario) AS quantidade_estados,
-                  COUNT(DISTINCT a.autor) AS quantidade_estados,
-                  COUNT(DISTINCT YEAR(p.Data_de_cadastro)) AS Anos
+                  COUNT(DISTINCT a.autor) AS quantidade_autores,
+                  COUNT(DISTINCT o.nome_orgao) AS quantidade_orgao,
+                  COUNT(DISTINCT YEAR(p.Data_de_cadastro)) AS Anos,
+                  MAX(p.Valor_Solicitado) AS maior_valor,
+                  MIN(p.Valor_Solicitado) AS menor_valor
                   FROM Processos p
                   INNER JOIN Beneficiario b ON p.beneficiario_id = b.id
-                  INNER JOIN Autor a ON p.autor_id = a.id;`;
+                  INNER JOIN Autor a ON p.autor_id = a.id
+                  INNER JOIN Orgao o ON p.orgao_id = o.id;`;
 
   connection.query(query, (error, results, fields) => {
     if (error) throw error;
